d74d5ca143bda094ee9e601e33359769b811535f
Atilla Süer <suer.atilla@gmail.com>
2023-01-24 20:37:15 +0300

    Proje dosyası ekle.

---
+++ AriBilgi.Blog.UI/wwwroot/Admin/pages/categoryAdd.js
@@ -0,0 +1,16 @@
+﻿
+
+app.controller("CategoryAddController", function ($scope, $http) {
+
+    $scope.CategoryAdd = function () {
+        $http({
+            method: "POST",
+            headers: GetHeader(),
+            url: "https://localhost:7071/api/Category/Add",
+            data: $scope.category
+        }).then(function (response) {
+            alert("Kategori başarıyla eklenmiştir.");
+        })
+    }
+
+});
